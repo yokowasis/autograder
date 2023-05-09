fe68e0fb35218eb2b7620354142698f5d74ce2be
Yokowasis - WSL - NITRO 5 <yokowasis@gmail.com>
2023-05-09 16:41:15 +0800

    case insensitive

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ function hitungnilai(
           if (bonus) {
             benar++;
             skor += bobotsoal;
-          } else if (kuncijawaban == jawabansoal) {
+          } else if (kuncijawaban.toLowerCase() === jawabansoal.toLowerCase()) {
             // Check Kunci PG
             benar++;
             skor += bobotsoal;
@@ -70,5 +70,57 @@ function hitungnilai(
     salah: salah.toFixed(2),
   };
 }
+
+console.log(
+  hitungnilai(
+    {
+      "0": "A",
+      "1": "A",
+      "2": "A",
+      "3": "A",
+      "4": "A",
+      "5": "A",
+      "6": "A",
+      "2001": "CHECK",
+      "2002": "CHECK",
+    },
+    {
+      "1": "a",
+      "2": "x",
+      "3": "x",
+      "4": "a",
+      "5": "x",
+      "6": "x",
+      "2001": "check",
+      "2002": "-check",
+      "2003": "-check",
+      "2004": "check",
+      "3001": "saturnus",
+      "5001": "check",
+      "5002": "-check",
+      "5003": "-check",
+      "5004": "check",
+      "6001": "saturnus",
+    },
+    {
+      "1": 16,
+      "2": 0,
+      "3": 0,
+      "4": 16,
+      "5": 0,
+      "6": 0,
+      "2001": 4,
+      "2002": 4,
+      "2003": 4,
+      "2004": 4,
+      "3001": 16,
+      "5001": 4,
+      "5002": 4,
+      "5003": 4,
+      "5004": 4,
+      "6001": 16,
+    }
+  )
+);
 
 export { calculateScore, hitungnilai, extractNumber };
